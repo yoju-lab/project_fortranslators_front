83fc9e3c2e754c1f341077695d2062d43c1ac064
Sanghun Oh <otter.oh@gmail.com>
2022-07-10 16:39:27 +0900

    add props

--- src/components/DoTranslation/DoTranslation.vue
+++ src/components/DoTranslation/DoTranslation.vue
@@ -8,7 +8,7 @@
 
     <!-- text areas -->
     <div class="row">
-      <TextAreas />
+      <TextAreas sourceTexts="sourceTexts" />
     </div>
 
     <!-- progress state -->
@@ -22,9 +22,13 @@
 import CurrentTerms from "./TranslatedCurrentTerms.vue";
 import TextAreas from "./TranslatePlace.vue";
 import ProgressState from "./ProgressState.vue";
+import { onMounted } from "vue";
 export default {
   data() {
-    return {};
+    return {
+      sourceTexts:
+        'Now -- as Russia\'s economic isolation in the wake of its invasion of Ukraine pushes it closer to Beijing -- that is changing, with fanfare. The bridge would "create a new channel connecting the two countries," Xi said during the call, which took place on his 69th birthday. "The Chinese side stands ready to work with the Russian side to push for steady and long-term development of practical bilateral cooperation," Xi said.',
+    };
   },
   components: {
     ProgressState,
--- src/components/DoTranslation/TranslatePlace.vue
+++ src/components/DoTranslation/TranslatePlace.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-6">
     <!-- source texts -->
-    <SourceTexts />
+    <SourceTexts sourceTexts=/>
   </div>
   <div class="col-6">
     <!-- translate Texts -->
@@ -13,6 +13,7 @@
 import SourceTexts from "./SourceTexts.vue";
 import TranslateTexts from "./TranslatingTexts.vue";
 export default {
+  props: [sourceTexts],
   components: {
     SourceTexts,
     TranslateTexts,
