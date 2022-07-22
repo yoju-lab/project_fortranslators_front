4ef1807eea2f200dfeae9ebfe8eabd92ca82bae8
Sanghun Oh <otter.oh@gmail.com>
2022-07-22 17:01:05 +0900

    add APITranslation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import AppTranslation from "./components/DoTranslation/DoTranslation.vue";
+import DoTranslation from "./components/DoTranslation/DoTranslation.vue";
+import ApiTranslation from "./components/ApiTranslation/ApiTranslation.vue";
+import { reactive } from "@vue/reactivity";
+
+const states = reactive({
+  currentMenu: "ApiTranslation",
+});
 </script>
 
 <template>
-  <AppTranslation />
+  <component :is="states.currentMenu"></component>
 </template>
 
 <style>
