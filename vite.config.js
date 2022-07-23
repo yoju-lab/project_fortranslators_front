c458fb5d8cdd788f4b933b90f4ce31d2d325d50d
Sanghun Oh <otter.oh@gmail.com>
2022-07-23 21:37:04 +0900

    add build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+// module.exports = {
+//   publicPath:
+//     process.env.NODE_ENV === "production" ? "/project_fortranslators" : "/",
+//   outputDir: "docs",
+// };
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: "",
+  root: "",
+  build: {
+    base: "/project_fortranslators",
+    outDir: "docs",
+  },
+});
